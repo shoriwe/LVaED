ba39fc99cec52c93ca84a72a73f036362318b2f4
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2021-02-17 10:30:42 -0500

    Update presentation.py

--- blueprints/presentation.py
+++ blueprints/presentation.py
@@ -11,3 +11,11 @@ def presentation():
                                  page_name="Presentation",
                                  body_page="presentation/navigation.html"
                                  )
+
+
+@presentation_blueprint.route("/presentation/list")
+def presentation_list():
+    return flask.render_template("basic/page.html",
+                                 page_name="List",
+                                 body_page="presentation/article.html",
+                                 content=flask.current_app.config["articles"].list())
