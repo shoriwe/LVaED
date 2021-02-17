b74568b03b5269e69ef453d90402ad97e5ce00a6
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2021-02-17 10:29:55 -0500

    Update main.py
    - Render the Markdown to HTML approaching GitHub's API

--- main.py
+++ main.py
@@ -1,13 +1,54 @@
+import os
+
 import flask
+import requests
 
 import blueprints.home
 import blueprints.presentation
+
+
+class Articles(object):
+    def __init__(self, list_article_html: str):
+        self.__list = list_article_html
+
+    def list(self):
+        return self.__list
+
+
+def render_article(article_path: str, output_path: str) -> str:
+    if not os.path.exists(output_path):
+        article = open(article_path)
+        headers = {
+            "Accept": "application/vnd.github.v3+json",
+        }
+        data = {"text": article.read()}
+        article.close()
+        response = requests.post(
+            "https://api.github.com/markdown",
+            json=data,
+            headers=headers
+        )
+        file = open(output_path, "w")
+        file.write(response.text)
+        file.flush()
+        file.close()
+    file = open(output_path)
+    html = file.read()
+    file.close()
+    return html
+
+
+def load_articles(app: flask.Flask):
+    app.config["articles"] = Articles(
+        render_article("articles/markdown/list.md", "articles/html/list.html")
+    )
 
 
 def setup() -> flask.Flask:
     app = flask.Flask(__name__, template_folder="templates")
     app.register_blueprint(blueprints.home.home_blueprint)
     app.register_blueprint(blueprints.presentation.presentation_blueprint)
+    load_articles(app)
     return app
 
 
