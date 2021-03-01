912722942a2b710ed0af3855a6aafbd1f4a142e9
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2021-03-01 13:49:57 -0500

    Create example.py
    - Initial implementation of the example module

---
+++ blueprints/example.py
@@ -0,0 +1,24 @@
+import io
+
+import flask
+
+__all__ = ["example_blueprint"]
+
+example_blueprint = flask.Blueprint("Example", "Example")
+
+
+@example_blueprint.route("/examples/download/<library>")
+def download(library: str):
+    if library == "c":
+        return flask.send_file(io.BytesIO(flask.current_app.config["libraries"].c()), attachment_filename="c.zip")
+    elif library == "java":
+        return flask.send_file(io.BytesIO(flask.current_app.config["libraries"].java()),
+                               attachment_filename="java.zip")
+    elif library == "python":
+        return flask.send_file(io.BytesIO(flask.current_app.config["libraries"].python()),
+                               attachment_filename="python.zip")
+    elif library == "all":
+        return flask.send_file(io.BytesIO(flask.current_app.config["libraries"].all()),
+                               attachment_filename="all.zip")
+    else:
+        return flask.redirect("/home")
