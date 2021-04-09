9efd0941df41a78e42c2d45bed3547bb8d7cf646
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2021-04-09 09:45:53 -0500

    Binary Tree Java Example And code cleanup

--- DataTypes/Python/stacks/list_stack.py
+++ DataTypes/Python/stacks/list_stack.py
@@ -2,55 +2,55 @@ import lists.double_linked_list
 
 
 class ListStack(object):
-    def __init__(self):
-        self.length = 0
-        self.content = lists.double_linked_list.DoublyLinkedList()
-        self.reversed = False
+	def __init__(self):
+		self.length = 0
+		self.content = lists.double_linked_list.DoublyLinkedList()
+		self.reversed = False
 
-    def update(self):
-        self.length = self.content.length
+	def update(self):
+		self.length = self.content.length
 
-    def reverse(self):
-        self.reversed = not self.reversed
+	def reverse(self):
+		self.reversed = not self.reversed
 
-    def clear(self):
-        self.content.clear()
+	def clear(self):
+		self.content.clear()
 
-    def is_empty(self) -> bool:
-        return self.content.length == 0
+	def is_empty(self) -> bool:
+		return self.content.length == 0
 
-    def peek(self):
-        if self.length == 0:
-            raise IndexError("The stack is empty")
-        if self.reversed:
-            return self.content[self.content.length - 1]
-        return self.content[0]
+	def peek(self):
+		if self.length == 0:
+			raise IndexError("The stack is empty")
+		if self.reversed:
+			return self.content[self.content.length - 1]
+		return self.content[0]
 
-    def pop(self):
-        if self.length == 0:
-            raise IndexError("The stack is empty")
-        if self.reversed:
-            value = self.content[self.content.length - 1]
-            del self.content[self.content.length - 1]
-            self.update()
-            return value
-        value = self.content[0]
-        del self.content[0]
-        self.update()
-        return value
+	def pop(self):
+		if self.length == 0:
+			raise IndexError("The stack is empty")
+		if self.reversed:
+			value = self.content[self.content.length - 1]
+			del self.content[self.content.length - 1]
+			self.update()
+			return value
+		value = self.content[0]
+		del self.content[0]
+		self.update()
+		return value
 
-    def push(self, obj: object):
-        if self.reversed:
-            self.content.append(obj)
-        else:
-            if self.length == 0:
-                self.content.append(obj)
-            else:
-                self.content.insert(0, obj)
-        self.update()
+	def push(self, obj: object):
+		if self.reversed:
+			self.content.append(obj)
+		else:
+			if self.length == 0:
+				self.content.append(obj)
+			else:
+				self.content.insert(0, obj)
+		self.update()
 
-    def size(self):
-        return self.length
+	def size(self):
+		return self.length
 
-    def search(self, obj: object) -> bool:
-        return obj in self.content
+	def search(self, obj: object) -> bool:
+		return obj in self.content
