d249b795fdd7b68c006de1c01d2e9aef1c701f02
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2021-04-11 14:35:18 -0500

    Better Transformation module
    - Added description to each section
    - Better code description
    - Better code recompiling

--- blueprints/transformations.py
+++ blueprints/transformations.py
@@ -19,91 +19,212 @@ __all__ = ["transformations_blueprint"]
 transformations_blueprint = flask.Blueprint("Transformation", "transformation")
 
 kinds = ["Script", "Module", "Class", "Function", "Class Method", "Base", "Argument", "ImportFrom",
-		 "ImportFromTarget"]
+         "ImportFromTarget"]
 
 
-def compile_code(code_tree: dict) -> str:
-	nodes = code_tree["nodes"].copy()
-	nodes.sort(key=lambda n: n["id"])
-	code = ""
-	in_class_context = False
-	in_import_from_context = False
-	in_function_context = False
-	in_class_method_context = False
-	defining_class_bases = False
-	for node in nodes[1:]:
-		if (not in_class_method_context) and (not defining_class_bases) and in_class_context and \
-				(node["kind"] != "Class Method"):
-			in_class_method_context = False
-			in_class_context = False
-			in_function_context = False
-			code += "\n\tpass"
-		elif in_class_context and defining_class_bases and node["kind"] != "Base":
-			if code[-1] == ',':
-				code = code[:-1]
-			code += "):"
-			defining_class_bases = False
-		elif in_class_method_context and node["kind"] != "Argument":
-			if code[-1] == ',':
-				code = code[:-1]
-			code += "):\n\t\tpass"
-			in_class_method_context = False
-			if node["kind"] != "Class Method":
-				in_class_context = False
-				code += "\n\tpass"
-		elif in_function_context and node["kind"] != "Argument":
-			if code[-1] == ',':
-				code = code[:-1]
-			code += "):\n\tpass"
-			in_function_context = False
-		elif in_import_from_context and node["kind"] != "ImportFromTarget":
-			if code[-1] == ',':
-				code = code[:-1]
+def get_parent(node: dict, links: dict):
+	parent = tuple(filter(lambda node_id: node["id"] in links[node_id], links.keys()))[0]
+	links[parent].remove(node["id"])
+	return parent
+
+
+class Compiler(object):
+	def __init__(self, code_tree: dict):
+		self.code_tree = code_tree
+		self.nodes = self.code_tree["nodes"].copy()
+		self.cursor = 1
+		self.number_of_nodes = len(self.nodes)
+		self.nodes.sort(key=lambda node: node["id"])
+		self.links = {}
+		for raw_link in map(lambda link: (link["source"], link["target"]), self.code_tree["links"]):
+			if raw_link[0] not in self.links.keys():
+				self.links[raw_link[0]] = [raw_link[1]]
+			else:
+				self.links[raw_link[0]].append(raw_link[1])
+
+	def compile_import_from(self, node):
+		children = []
+		while self.cursor < self.number_of_nodes:
+			child_node = self.nodes[self.cursor]
+			if child_node["kind"] != "ImportFromTarget":
+				break
+			self.cursor += 1
+			children.append(child_node["name"])
+		return f"from {node['name']} import " + ", ".join(children)
+
+	def compile_module(self, node):
+		return f"import {node['name']}"
+
+	def compile_class(self, node):
+		# Add bases
+		bases = []
+		while self.cursor < self.number_of_nodes:
+			base_node = self.nodes[self.cursor]
+			if base_node["kind"] != "Base":
+				break
+			self.cursor += 1
+			bases.append(base_node["name"])
+		# Add Class Methods
+		class_methods = []
+		while self.cursor < self.number_of_nodes:
+			class_method_node = self.nodes[self.cursor]
+			if class_method_node["kind"] != "Class Method":
+				break
+			self.cursor += 1
+			class_method = [class_method_node["name"]]
+			while self.cursor < self.number_of_nodes:
+				class_method_argument = self.nodes[self.cursor]
+				if class_method_argument["kind"] != "Argument":
+					break
+				self.cursor += 1
+				class_method.append(class_method_argument["name"])
+			class_methods.append(class_method)
+		result = f"class {node['name']}"
+		if bases:
+			result += "(" + ", ".join(bases) + ")"
+		result += ":"
+		if class_methods:
+			for class_method in class_methods:
+				result += f"\n\n\tdef {class_method[0]}(" + ", ".join(class_method[1:]) + "):\n\t\tpass"
+		else:
+			result += "\n\tpass"
+		return result
+
+	def compile_function(self, node):
+		arguments = []
+		while self.cursor < self.number_of_nodes:
+			argument = self.nodes[self.cursor]
+			if argument["kind"] != "Argument":
+				break
+			self.cursor += 1
+			arguments.append(argument["name"])
+		return f"def {node['name']}(" + ", ".join(arguments) + "\n\tpass"
+
+	def compile_code(self) -> str:
+		code = ""
+		while self.cursor < self.number_of_nodes:
+			node = self.nodes[self.cursor]
+			self.cursor += 1
+			if node["kind"] == "ImportFrom":
+				code += self.compile_import_from(node)
+			elif node["kind"] == "Module":
+				code += self.compile_module(node)
+			elif node["kind"] == "Class":
+				code += "\n\n" + self.compile_class(node)
+			elif node["kind"] == "Function":
+				code += "\n\n" + self.compile_function(node)
+			else:
+				continue
+			# print(node["kind"])
 			code += "\n"
-			in_import_from_context = False
-		if node["kind"] == "Base":
-			code += f"{node['name']},"
-		elif node["kind"] == "ImportFrom":
-			in_import_from_context = True
-			code += f"\nfrom {node['name']} import "
-		elif node["kind"] == "ImportFromTarget":
-			code += f"{node['name']},"
-		elif node["kind"] == "Module":
-			code += f"\nimport {node['name']}"
-		elif node["kind"] == "Class":
-			code += f"\n\n\nclass {node['name']}("
-			in_class_context = True
-			defining_class_bases = True
-		elif node["kind"] == "Class Method":
-			code += f"\n\n\tdef {node['name']}("
-			in_class_method_context = True
-		elif node["kind"] == "Function":
-			in_function_context = True
-			code += f"\n\n\ndef {node['name']}("
-		elif node["kind"] == "Argument":
-			code += f"{node['name']},"
-	if in_function_context:
-		if code[-1] == ',':
-			code = code[:-1]
-		code += "):\n\tpass"
-	if in_class_method_context:
-		if code[-1] == ',':
-			code = code[:-1]
-		code += "):\n\t\tpass"
-	if in_class_context:
-		code += "\n\tpass"
-	return code
+		return code
 
 
-def list_objects(raw_json_graph: str) -> str:
-	result = ""
-	try:
-		json_graph = json.loads(raw_json_graph)
-		for node in json_graph["nodes"]:
-			result += node["kind"] + "|" + node["name"].replace("_", "\\_") + "\n"
-	except Exception:
-		pass
-	return result
+class ListObjects(object):
+	def __init__(self, raw_code_tree: str):
+		code_tree = json.loads(raw_code_tree)
+		self.imports = "ID | Source | Names\n ---- | ---- | ----"
+		self.updated_imports = False
+		self.classes = "ID | Class Name | Bases | Methods\n ----- | ----- | ----- | -----"
+		self.updated_classes = False
+		self.functions = "ID | Function Name | Arguments\n ----- | ----- | -----"
+		self.updated_functions = False
+		self.code_tree = code_tree
+		self.nodes = self.code_tree["nodes"].copy()
+		self.cursor = 1
+		self.number_of_nodes = len(self.nodes)
+		self.nodes.sort(key=lambda node: node["id"])
+		self.links = {}
+		for raw_link in map(lambda link: (link["source"], link["target"]), self.code_tree["links"]):
+			if raw_link[0] not in self.links.keys():
+				self.links[raw_link[0]] = [raw_link[1]]
+			else:
+				self.links[raw_link[0]].append(raw_link[1])
+
+	def describe_import_from(self, node: dict):
+		names = []
+		while self.cursor < self.number_of_nodes:
+			import_name = self.nodes[self.cursor]
+			if import_name["kind"] != "ImportFromTarget":
+				break
+			self.cursor += 1
+			names.append(import_name["name"].replace("_", "\\_"))
+		name = node['name'].replace("_", "\\_")
+		self.imports += f"\n{node['id']} | {name} | " + ", ".join(names)
+
+	def describe_module(self, node: dict):
+		name = node['name'].replace("_", "\\_")
+		self.imports += f"\n{node['id']} | - | {name}"
+
+	def describe_class(self, node: dict):
+		# Add bases
+		bases = []
+		while self.cursor < self.number_of_nodes:
+			base_node = self.nodes[self.cursor]
+			if base_node["kind"] != "Base":
+				break
+			self.cursor += 1
+			bases.append(base_node["name"].replace("_", "\\_"))
+		# Add Class Methods
+		class_methods = []
+		while self.cursor < self.number_of_nodes:
+			class_method_node = self.nodes[self.cursor]
+			if class_method_node["kind"] != "Class Method":
+				break
+			self.cursor += 1
+			class_method = [class_method_node["name"].replace("_", "\\_")]
+			while self.cursor < self.number_of_nodes:
+				class_method_argument = self.nodes[self.cursor]
+				if class_method_argument["kind"] != "Argument":
+					break
+				self.cursor += 1
+				class_method.append(class_method_argument["name"].replace("_", "\\_"))
+			class_methods.append(class_method)
+		name = node["name"].replace("_", "\\_")
+		self.classes += f"\n{node['id']} | {name} | " + ", ".join(bases) + " | " + "<br> ".join(
+			f"{class_method[0]}\\(" + ", ".join(class_method[1:]) + ")" for class_method in class_methods)
+
+	def describe_function(self, node: dict):
+		arguments = []
+		while self.cursor < self.number_of_nodes:
+			argument = self.nodes[self.cursor]
+			if argument["kind"] != "Argument":
+				break
+			self.cursor += 1
+			arguments.append(argument["name"].replace("_", "\\_"))
+		name = node['name'].replace("_", "\\_")
+		self.functions += f"\n{node['id']} | {name} | " + "<br> ".join(arguments)
+
+	def generate_code_description(self):
+		result = ""
+		if self.updated_imports:
+			result += "## Script imports\n\n\n" + self.imports + "\n\n\n\n"
+		if self.updated_classes:
+			result += "## Script defined classes\n\n\n" + self.classes + "\n\n\n\n"
+		if self.updated_functions:
+			result += "## Script defined functions\n\n\n" + self.functions + "\n\n\n\n"
+		return result
+
+	def list_objects(self) -> str:
+		while self.cursor < self.number_of_nodes:
+			node = self.nodes[self.cursor]
+			self.cursor += 1
+			if node["kind"] == "ImportFrom":
+				self.updated_imports = True
+				self.describe_import_from(node)
+			elif node["kind"] == "Module":
+				self.updated_imports = True
+				self.describe_module(node)
+			elif node["kind"] == "Class":
+				self.updated_classes = True
+				self.describe_class(node)
+			elif node["kind"] == "Function":
+				self.updated_functions = True
+				self.describe_function(node)
+			else:
+				continue
+		# print(node["kind"])
+		return self.generate_code_description()
 
 
 @transformations_blueprint.route("/transformations", methods=("GET", "POST"))
@@ -193,7 +314,7 @@ def transformations():
 				try:
 					raw_graph_data = json.loads(content)
 					graph_data_manager = networkx.readwrite.json_graph.node_link_graph(raw_graph_data)
-					source_code = compile_code(raw_graph_data)
+					source_code = Compiler(raw_graph_data).compile_code()
 					graph_title = raw_graph_data["nodes"][0]["name"]
 					json_graph = json.dumps(raw_graph_data)
 					success = True
@@ -202,11 +323,11 @@ def transformations():
 		if success:
 			network_graph = from_networkx(graph_data_manager, networkx.spring_layout, scale=100, center=(0, 0))
 			network_graph.node_renderer.glyph = bokeh.models.Circle(size=15,
-																	fill_color=bokeh.transform.factor_cmap(
-																		"kind",
-																		bokeh.palettes.Category20[20],
-																		factors=kinds)
-																	)
+			                                                        fill_color=bokeh.transform.factor_cmap(
+				                                                        "kind",
+				                                                        bokeh.palettes.Category20[20],
+				                                                        factors=kinds)
+			                                                        )
 
 			network_graph.edge_renderer.glyph = bokeh.models.MultiLine(
 				line_alpha=0.5,
@@ -214,26 +335,23 @@ def transformations():
 				line_join='miter',
 			)
 			plot = bokeh.plotting.figure(tooltips=[("Object Name", "@name"), ("Kind", "@kind")],
-										 tools="pan,wheel_zoom,save,reset",
-										 x_range=bokeh.models.Range1d(-150, 150),
-										 y_range=bokeh.models.Range1d(-150, 150),
-										 title=graph_title)
+			                             tools="pan,wheel_zoom,save,reset",
+			                             x_range=bokeh.models.Range1d(-150, 150),
+			                             y_range=bokeh.models.Range1d(-150, 150),
+			                             title=graph_title)
 			plot.renderers.append(network_graph)
 			script, div = bokeh.embed.components(plot)
-			listed_objects = list_objects(json_graph)
 			return flask.render_template(
 				"basic/page.html",
 				page_name="Transformations",
 				body_page="transformations/analyser.html",
-				listed_objects=markdown.markdown(
-					"Name | Metalanguage Kind | AST Kind \n"
-					"---- | ---- | ----\n" + listed_objects + "\n",
-					extensions=["codehilite", "tables"]),
+				listed_objects=markdown.markdown(ListObjects(json_graph).list_objects(),
+				                                 extensions=["codehilite", "extra"]),
 				metalanguage_source_code=markdown.markdown(
 					"# Metalanguage\n\nGenerated metalanguage useful for sharing the graph or regenerate a code "
 					f"interface for future use. The metalanguage was stored in the JSON bellow.\n\n\t:::json\n\t{json_graph}"
 					"\n\n# Source Code\n\n\t:::python\n\t" + source_code.replace("\n", "\n\t"),
-					extensions=["codehilite"]),
+					extensions=["codehilite", "extra"]),
 				plot_script=script,
 				plot_div=div,
 				js_resources=bokeh.resources.INLINE.render_js(),
